4cfafcaaa9970abe931801f837cc1f66b6803478
JBrLloyd <jackson@jbrlloyd.dev>
2023-04-14 16:23:25 +1000

    Update setup.ps1

--- os/windows/setup.ps1
+++ os/windows/setup.ps1
@@ -1,4 +1,7 @@
 winget install `
   Kubernetes.minikube 
   TortoiseSVN.TortoiseSVN `
+  starship `
   --accept-package-agreements
+
+"Invoke-Expression (&starship init powershell)" | Out-File -Append $PROFILE
