3b1a1334475450f1fed0efeae7376e56c90d4e4b
JBrLloyd <jackson@jbrlloyd.dev>
2023-04-14 15:39:26 +1000

    Update ubuntu-setup.sh

--- os/linux/ubuntu-setup.sh
+++ os/linux/ubuntu-setup.sh
@@ -1,5 +1,6 @@
 # Ubuntu
 
+sudo add-apt-repository ppa:daniel-milde/gdu
 sudo apt update
 sudo apt install \
   curl \
@@ -8,9 +9,19 @@ sudo apt install \
   zsh \
   zsh-antigen \
   tmux \
-  fzf
+  fzf \
+  build-essential \
+  neovim \
+  ripgrep \
+  gdu
 
 chsh -s $(which zsh)
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit*
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
@@ -35,4 +46,10 @@ curl -SL https://raw.githubusercontent.com/JBrLloyd/machine-setup/main/app_confi
 curl -SL https://raw.githubusercontent.com/JBrLloyd/machine-setup/main/app_configs/bash/.bashrc -o ~/.bashrc
 curl -SL https://raw.githubusercontent.com/JBrLloyd/machine-setup/main/app_configs/starship/starship.toml -o ~/.config/starship.toml
 
+export PATH="~/.cargo/bin:$PATH"
+
 /usr/bin/zsh
+
+cargo install bottom --locked
+cargo install tree-sitter-cli
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
